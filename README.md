66f99f1cbab2abcb81d0e1f705f59fc0433efa91
intetsu2 <86745375+intetsu2@users.noreply.github.com>
2022-03-11 14:48:57 -0700

    Many changes and upgrades

---
+++ README.md
@@ -0,0 +1,12 @@
+# State and Carrier Finder
+### Small python project that finds the state and the carrier of a phone number.
+
+## Instructions:
+- Run the code
+- Enter phone number with area code included
+- The you will the Location and Carrier
+
+## Results should look similar to this after
+![Terminal Example](terminal_screenshot.jpg)
+
+### NOTE: the carrier results might return not found due to carrier data not being specified.
--- numfounder.py
+++ numfounder.py
@@ -1,15 +1,21 @@
 "This program finds the location of a phone number and it's carrier"
-
-# importion neede
+# importion needed
 import phonenumbers
 from phonenumbers import geocoder
 from phonenumbers import carrier
+from phonenumbers import carrierdata
 
 # variables neede
 num = input("Enter number with area code: ") # asks the user number with it's area code
 p_num = phonenumbers.parse(num, "CH")
 ser_num = phonenumbers.parse(num, "RO")
 
+# location and carrier
+location = geocoder.description_for_number(p_num, "en")
+carrier = carrier.name_for_number(ser_num, "en")
+if carrier == "":
+    carrier = 'Not found'
+
 # printing process
-print(geocoder.description_for_number(p_num, "en"))
-print(carrier.name_for_number(ser_num, "en"))
+print(f'{num} is located in {location}')
+print(f'The carrier of {num} is {carrier}')
